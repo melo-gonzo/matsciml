43c7e84149e334406f385371c58d9d67d0b6160f
Gonzales, Carmelo <carmelo.gonzales@intel.com>
2024-02-29 12:44:55 -0800

    fixing up faenet to work with ForceRegressionTask, using frame averaging stochastic or all modes

--- matsciml/datasets/transforms/__init__.py
+++ matsciml/datasets/transforms/__init__.py
@@ -4,3 +4,4 @@ from matsciml.datasets.transforms.pbc import *
 from matsciml.datasets.transforms.props import *
 from matsciml.datasets.transforms.representations import *
 from matsciml.datasets.transforms.matgl_datasets import *
+from matsciml.datasets.transforms.frame_averaging import FrameAveraging
