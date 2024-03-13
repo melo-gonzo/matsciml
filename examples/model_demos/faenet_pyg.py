50318e7fa5ed0016afb673eba708629305fb9842
santiago.miret <smiret@aipg-ra-skx-03.ra.intel.com>
2024-03-13 14:43:45 -0700

    fanet-mp-traj

--- examples/model_demos/faenet_pyg.py
+++ examples/model_demos/faenet_pyg.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-import pytorch_lightning as pl
+import sys
+sys.path.append("/workspace/ai-mat-top/matsciml_top/forks/carmelo_matsciml/")
 
 from matsciml.datasets.transforms import (
     FrameAveraging,
@@ -9,33 +10,76 @@ from matsciml.datasets.transforms import (
     UnitCellCalculator,
 )
 from matsciml.lightning.data_utils import MatSciMLDataModule
-from matsciml.models.base import ScalarRegressionTask, GradFreeForceRegressionTask
+from matsciml.models.base import ScalarRegressionTask, ForceRegressionTask, GradFreeForceRegressionTask, MultiTaskLitModule
 from matsciml.models.pyg import FAENet
+
+
+# Atomic Energies table
+import mendeleev
+import pytest
+import pytorch_lightning as pl
+from mendeleev.fetch import fetch_ionization_energies
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+import wandb
+from tqdm import tqdm
+from matsciml.lightning.callbacks import GradientCheckCallback
 
 """
 This example script runs through a fast development run of the IS2RE devset
 in combination with a PyG implementation of FAENet.
 """
 
-# construct IS2RE relaxed energy regression with PyG implementation of FAENet
-task = GradFreeForceRegressionTask(
+TRAIN_PATH = "/datasets-alt/molecular-data/mat_traj/mp-traj-full/train"
+VAL_PATH = "/datasets-alt/molecular-data/mat_traj/mp-traj-full/val"
+DATASET = "mp-traj"
+
+
+# Multi-Task with Scalar (Energy) and GradFreeForce (Force)
+
+task_energy = ScalarRegressionTask(
     encoder_class=FAENet,
     encoder_kwargs={
         "average_frame_embeddings": True,
         "pred_as_dict": False,
         "hidden_dim": 128,
-        "out_dim": 128,
+        "out_dim": 64,
         "tag_hidden_channels": 0,
     },
-    output_kwargs={"lazy": False, "input_dim": 128, "hidden_dim": 128},
-    task_keys=["force"],
+    output_kwargs={"lazy": False, "input_dim": 64, "hidden_dim": 64},
+    task_keys=["energy"],
+    lr=0.005,
 )
 
-# ### matsciml devset for OCP are serialized with DGL - this transform goes between the two frameworks
-dm = MatSciMLDataModule.from_devset(
-    "S2EFDataset",
+
+task_force = GradFreeForceRegressionTask(
+    encoder_class=FAENet,
+    encoder_kwargs={
+        "average_frame_embeddings": True,
+        "pred_as_dict": False,
+        "hidden_dim": 128,
+        "out_dim": 64,
+        "tag_hidden_channels": 0,
+    },
+    output_kwargs={"lazy": False, "input_dim": 64, "hidden_dim": 64},
+    # task_keys=["energy"],
+    lr=0.005,
+)
+
+task = MultiTaskLitModule(
+    ("MaterialsProjectDataset", task_energy),
+    ("MaterialsProjectDataset", task_force),
+)
+
+
+
+dm = MatSciMLDataModule(
+    "MaterialsProjectDataset",
+    train_path=TRAIN_PATH,
+    val_split=VAL_PATH,
     dset_kwargs={
         "transforms": [
+            UnitCellCalculator(),
             PointCloudToGraphTransform(
                 "pyg",
                 cutoff_dist=20.0,
@@ -45,12 +89,62 @@ dm = MatSciMLDataModule.from_devset(
         ],
     },
 )
+    # dm.setup()
+
+wandb.init(project='faenet_debug', entity='smiret', mode='online', )
+logger = WandbLogger(log_model="all", name=f"faenet-{DATASET}-data", save_dir='/workspace/nosnap/matsciml/mace_train')
+
+mc = ModelCheckpoint(monitor="val_energy", save_top_k=5)
 
 
 # run a quick training loop
-trainer = pl.Trainer(fast_dev_run=10, devices=1)
+trainer = pl.Trainer(accelerator="gpu",
+        devices=1,
+        min_epochs=20,
+        max_epochs=100,
+        log_every_n_steps=100,
+        strategy="ddp_find_unused_parameters_true",
+        logger=logger,
+        callbacks=[
+            GradientCheckCallback(),
+            mc,
+        ],)
 trainer.fit(task, datamodule=dm)
 
 
 ########################################################################################
 ########################################################################################
+
+
+# # construct Materials Project band gap regression with PyG implementation of FAENet
+# task = ScalarRegressionTask(
+#     encoder_class=FAENet,
+#     encoder_kwargs={
+#         "pred_as_dict": False,
+#         "hidden_dim": 128,
+#         "out_dim": 64,
+#         "tag_hidden_channels": 0,
+#         "input_dim": 128,
+#     },
+#     output_kwargs={"lazy": False, "input_dim": 64, "hidden_dim": 64},
+#     task_keys=["band_gap"],
+# )
+
+# dm = MatSciMLDataModule.from_devset(
+#     "MaterialsProjectDataset",
+#     dset_kwargs={
+#         "transforms": [
+#             UnitCellCalculator(),
+#             PointCloudToGraphTransform(
+#                 "pyg",
+#                 cutoff_dist=20.0,
+#                 node_keys=["pos", "atomic_numbers"],
+#             ),
+#             FrameAveraging(frame_averaging="3D", fa_method="stochastic"),
+#         ],
+#     },
+# )
+
+# # run a quick training loop
+# trainer = pl.Trainer(fast_dev_run=10)
+# trainer.fit(task, datamodule=dm)
