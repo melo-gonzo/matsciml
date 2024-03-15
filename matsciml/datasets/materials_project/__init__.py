4da760e669563bad41485837042290ec2ad20d76
Gonzales, Carmelo <carmelo.gonzales@intel.com>
2024-03-15 13:52:24 -0700

    hacking in a fake GnomeMaterialsProjectDataset

--- matsciml/datasets/materials_project/__init__.py
+++ matsciml/datasets/materials_project/__init__.py
@@ -2,6 +2,12 @@ from __future__ import annotations
 
 from importlib.util import find_spec
 
+from matsciml.datasets.materials_project.api import MaterialsProjectRequest
+from matsciml.datasets.materials_project.dataset import MaterialsProjectDataset, GnomeMaterialsProjectDataset
 
 if find_spec("torch_geometric") is not None:
-    pass
+    from matsciml.datasets.materials_project.dataset import (
+        CdvaeLMDBDataset,
+        PyGCdvaeDataset,
+        PyGMaterialsProjectDataset,
+    )
